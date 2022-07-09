42e69533d2cdf534d2bac74a78417440359da430
Dinki Yaduwanshi <91734640+anonymousdaisy5@users.noreply.github.com>
2022-07-09 13:06:10 +0530

    Create Character.java

---
+++ Character.java
@@ -0,0 +1,23 @@
+
+
+import java.util.Scanner;
+public class Solution {
+
+
+	public static void main(String[] args) {
+		
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i = 1;
+        while (i<=n){
+            int j =1;
+            while (j<=i){
+                System.out.print((char)('A'+i+j-2));
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    } }
+		
+	
