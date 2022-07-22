7df74a137ad433ea39ef21920bbbf55af749e5db
Dinki Yaduwanshi <91734640+anonymousdaisy5@users.noreply.github.com>
2022-07-22 21:02:49 +0530

    Update class.java

--- OOPS/class.java
+++ OOPS/class.java
@@ -20,9 +20,26 @@ public class Java1 {
 			
 		Student [] students= new Student[5];// here student is user defined data type
 		
-//		Student kunal;
+//		Student kunal; here kunal is acting as a reference variable and is stored in stack memory . 
 //		System.out.println(kunal.rno);
-		System.out.println(Arrays.toString(students));
-			
+		// OUTPUT- it gives an error saying local variable is not initialized
+		
+	//	System.out.println(Arrays.toString(students));
+		// OUTPUT-[null, null, null, null, null]
+			// ie. when not initialized student array returns with null values
+		
+		//HOW TO CREATE OBJECT- 
+		/* new keyword is required dynamically allocate memory and return reference to it which 
+		 is stored in the variable ie. original object is in heap , we can't access the memory address (not allowed in java)
+		 this is key to security in java that we cannot alter(manipulate) references in java as we can in c++ using pointers */
+		
+		Student student1 = new Student();
+		// all the lhs side is compile time and right hand side is runtime (dynamic memory allocation )
+		
+		
+		
+		
+		
 }
 	}
+
