91567a6423bf20d96b1ed9e7381a6e6d6b95be1c
Dinki Yaduwanshi <91734640+anonymousdaisy5@users.noreply.github.com>
2022-07-23 11:28:51 +0530

    Update Triangular Star Pattern.java

--- Triangular Star Pattern.java
+++ Triangular Star Pattern.java
@@ -48,6 +48,29 @@ int n ;
         System.out.println();
     }
 		
-	}
+	
 
-}
+
+
+// for left right triangle
+
+ class Trialeft {
+
+	public static void main(String[] args) {
+		
+		int n =6;
+      for(int i =0;i<=6;i++) {
+	 
+   	  for(int j=2*(n-i);j>=0 ;j--) {
+    		  System.out.print(" ");
+    	  }
+    	  
+   	  for(int j=0;j<=i;j++) {
+    		  System.out.print( " *");
+    	  }
+   	  System.out.println();
+	
+     }
+	
+	}
+ }
